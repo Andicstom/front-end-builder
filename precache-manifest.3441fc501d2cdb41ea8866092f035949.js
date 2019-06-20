08b9b65d3943c6085764ecb7bbca0cab422263f7
Tamás Andics <andicstom@gmail.com>
2019-06-20 20:05:57 +0200

    Updates

--- precache-manifest.3441fc501d2cdb41ea8866092f035949.js
+++ precache-manifest.3441fc501d2cdb41ea8866092f035949.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/front-end-builder/static/js/runtime~main.ae5cb415.js"
   },
   {
-    "revision": "e401213627756a0b1909",
-    "url": "/front-end-builder/static/js/main.157bcfbe.chunk.js"
+    "revision": "8b48d279fb57d4d7614c",
+    "url": "/front-end-builder/static/js/main.382c113f.chunk.js"
   },
   {
     "revision": "105cecaec92132b66170",
     "url": "/front-end-builder/static/js/2.30d7af93.chunk.js"
   },
   {
-    "revision": "e401213627756a0b1909",
+    "revision": "8b48d279fb57d4d7614c",
     "url": "/front-end-builder/static/css/main.c22ff9d7.chunk.css"
   },
   {
-    "revision": "14d5672743ccfff90728bba5c58ad81a",
+    "revision": "44b48d97e73335ed5ec0a3df0a5a902a",
     "url": "/front-end-builder/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/front-end-builder/precache-manifest.59dffc7f15eb715ffc578b7b903ed796.js"
+  "/front-end-builder/precache-manifest.3441fc501d2cdb41ea8866092f035949.js"
 );
 
 workbox.clientsClaim();
